0db1681e6a6ba114d60d76b2721e081a2b2c689e
Oribiel <153157241+Oribiel@users.noreply.github.com>
2024-06-30 05:20:32 -0600

    Frm
    Creados

---
+++ JuatsappPresentacion/src/main/java/Utilerias/ImagePanel.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utilerias;
+
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ImagePanel extends JPanel {
+    private Image backgroundImage;
+
+    public ImagePanel(String fileName) {
+        try {
+            backgroundImage = new ImageIcon(fileName).getImage();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+}
+
